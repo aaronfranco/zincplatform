fcec5e11b45b314ad21f7681accaf2dc2ab97b1f
Aaron Franco <aarongfranco@gmail.com>
2018-01-10 12:47:06 -0500

    Setting up basic website with bootstrap 4. Next step is to create the API requests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,8 +2,20 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get products_index_url
+    get "/"
     assert_response :success
   end
-
+  test "index should display all products" do
+    get "/"
+    assert_select "div.product-card", 2
+  end
+  test "index should display product name accurately" do
+    get "/"
+    assert_select "h1.product-name", "MyString"
+  end
+  test "index should display product price accurately" do
+    get "/"
+    # TODO: We should also test the currency type if/when we have to localize the app
+    assert_select "h2.product-price", "$9.99"
+  end
 end
